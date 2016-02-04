f347045ac604a49a2eb485ef4c3e02ccb4c8f948
dominicxcliu <dominicxcliu@gmail.com>
2016-02-03 21:54:18 -0500

    grid, row and col checking (hacky implementation)

---
+++ SudokuV1/Assets/GameController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public Square[,] grid;
+	public GameObject[] subGrids;
+
+
+	// Use this for initialization
+	void Start () {
+		grid = new Square [9, 9];
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				grid[i,j] = (Physics.OverlapSphere(new Vector3((-8f + 2f*i), (-8f + 2f*j), 0f), 0.01f))[0].gameObject.GetComponent<Square>();
+//				grid [i, j].active = true;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		grid [1, 2].active = true;
+		checkCols();
+		checkRows();
+		checkSubs ();
+	}
+
+	void checkCols() {
+		for (int i = 0; i < 9; i++) {
+			bool[] checker = new bool[9];
+			for (int j = 0; j < 9; j++) {
+				Square curr = grid [i, j];
+				if (curr.number == 0) {
+					//donothing
+				} else if (checker [(curr.number - 1)]) {
+					for (int k = 0; k < 9; k++) {
+						grid [i, k].wrong = true;
+					}
+				} else {
+					checker [(curr.number - 1)] = true;
+				}
+			}
+		}
+	}
+
+	void checkRows() {
+		for (int j = 0; j < 9; j++) {
+			bool[] checker = new bool[9];
+			for (int i = 0; i < 9; i++) {
+				Square curr = grid [i, j];
+				if (curr.number == 0) {
+					//donothing
+				} else if (checker [(curr.number - 1)]) {
+					for (int k = 0; k < 9; k++) {
+						grid [k, j].wrong = true;
+					}
+				} else {
+					checker [(curr.number - 1)] = true;
+				}
+			}
+		}
+	}
+
+	void checkSubs() {
+		
+	}
+}
---
+++ SudokuV1/Assets/Square.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Square : MonoBehaviour {
+
+	public bool active;
+	private TextMesh tm;
+	private Renderer myRenderer;
+	public int number;
+	public bool wrong;
+
+	// Use this for initialization
+	void Start () {
+		active = false;
+		tm = this.gameObject.GetComponentInChildren<TextMesh> ();
+		myRenderer = this.gameObject.GetComponent<Renderer> ();
+		tm.text = "";
+		number = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (number == 0) {
+			tm.text = "";
+		}
+		else {
+			tm.text = number.ToString ();
+		}
+
+		myRenderer.material.color = Color.white;
+		if (wrong) {
+			myRenderer.material.color = Color.red;
+		}
+
+		if (active) {
+			myRenderer.material.color = Color.black;
+			Debug.Log ("I am active");
+			if (Input.GetKeyDown (KeyCode.Alpha1)) {
+				number = 1;
+				active = false;
+			} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+				number = 2;
+				active = false;
+			} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+				number = 3;
+				active = false;
+			} else if (Input.GetKeyDown (KeyCode.Alpha4)) {
+				number = 4;
+				active = false;
+			} else if (Input.GetKeyDown (KeyCode.Alpha5)) {
+				number = 5;
+				active = false;
+			} else if (Input.GetKeyDown (KeyCode.Alpha6)) {
+				number = 6;
+				active = false;
+			} else if (Input.GetKeyDown (KeyCode.Alpha7)) {
+				number = 7;
+				active = false;
+			} else if (Input.GetKeyDown (KeyCode.Alpha8)) {
+				number = 8;
+				active = false;
+			} else if (Input.GetKeyDown (KeyCode.Alpha9)) {
+				number = 9;
+				active = false;
+			} else if (Input.GetKeyDown (KeyCode.Backspace)) {
+				number = 0;
+				active = false;
+			} else {
+				return;
+			}
+			
+		}
+	}
+
+	void OnMouseDown() {
+		Debug.Log ("on mouse down");
+		active = true;
+	}
+}
